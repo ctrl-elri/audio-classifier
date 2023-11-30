8ab2951dc68b106e069cdd2ee572f2c33831fa8b
Ella Rinnemaa <ellarinnemaa@gmail.com>
2023-11-30 13:55:04 +0200

    create structure with comments

--- project_peltola_rinnemaa.py
+++ project_peltola_rinnemaa.py
@@ -1,2 +1,62 @@
 import numpy as np
 import librosa as lb
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC  # Example classifier, use any desired classifier
+
+# 1. Data collection (Assuming you have a list of audio files)
+# You might use a library like librosa to handle audio data.
+audio_files = [...]  # List of paths to audio files
+
+# 2. Data conversion to wav file (If necessary, convert to WAV format)
+# This step may not be needed if your audio files are already in WAV format.
+
+# 3. Normalize the data
+# Normalize audio data using librosa or other signal processing libraries.
+normalized_data = []
+for file in audio_files:
+    data, sr = librosa.load(file)
+    normalized = librosa.util.normalize(data)
+    normalized_data.append(normalized)
+
+# 4. Extract time-domain features
+# Extract time-domain features using librosa or other signal processing libraries.
+time_features = []
+for data in normalized_data:
+    # Extract time-domain features (e.g., zero-crossing rate, rms, etc.)
+    # Append features to the time_features list.
+
+# 5. Extract frequency-domain features
+# Use Fourier Transform to convert data to frequency domain and extract features.
+frequency_features = []
+for data in normalized_data:
+    # Apply Fourier Transform and extract frequency-domain features
+    # Append features to the frequency_features list.
+
+# 6. Plot spectrograms and MFCC from some of the files and compare them
+# Use matplotlib or other plotting libraries to visualize spectrograms and MFCC.
+# This requires computing Mel-frequency cepstral coefficients (MFCC) and spectrograms.
+# Compare and plot them to observe differences.
+
+# 7. Implement the classifier (Choose a machine learning model)
+
+
+# Prepare feature matrix
+X = []  # Combine time and frequency features into X
+y = [...]  # Labels for your data
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize and train the classifier
+classifier = SVC()
+classifier.fit(X_train, y_train)
+
+# 8. Train the model (Already part of the previous step)
+
+# 9. Calculate results and analyze them
+# Use the trained model to predict on test data and analyze results
+predictions = classifier.predict(X_test)
+# Perform evaluation, calculate accuracy, confusion matrix, etc.
+
+# 10. Write report
+# Summarize your findings, results, and analysis in a report format using markdown or a document editor.
