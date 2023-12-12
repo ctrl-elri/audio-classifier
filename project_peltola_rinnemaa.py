0e345c2b7060e40c3eb127d392a1f702d88c4f4a
Ella Rinnemaa <ellarinnemaa@gmail.com>
2023-12-12 15:48:24 +0200

    Create class and delete unfit samples

--- project_peltola_rinnemaa.py
+++ project_peltola_rinnemaa.py
@@ -1,5 +1,7 @@
+
 import numpy as np
 import librosa as lb
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC 
 from sklearn.metrics import accuracy_score, confusion_matrix
@@ -7,81 +9,92 @@ import sounddevice as sd
 import soundfile as sf
 from scipy.signal import spectrogram
 import matplotlib.pyplot as plt
-import os
 
-# Reading audio data and storing samples in separate lists
+class AudioProcessor:
+    def __init__(self, hop_length=256, frame_length=512):
+        self.hop_length = hop_length
+        self.frame_length = frame_length
 
-def read_file(file):
-    audio, fs = lb.load(file, sr=None)
-    return audio, fs   
+    def read_file(self, file):
+        try:
+            audio, fs = lb.load(file, sr=None)
+            return audio, fs
+        except Exception as e:
+            print(f"Error reading {file}: {e}")
+            return None, None
 
-def get_bus_samples():
-    bus_samp = []
+    def read_files_from_folder(self, folder_name):
+        samples = []
+        for filename in os.listdir(folder_name):
+            file_path = os.path.join(folder_name, filename)
+            if os.path.isfile(file_path):
+                audio, fs = self.read_file(file_path)
+                if audio is not None:
+                    samples.append((audio, fs))
+        return samples
 
-    for filename in os.listdir('bus_samples'):
-        file_path = os.path.join('bus_samples', filename)
-        if os.path.isfile(file_path):
-            audio, fs = read_file(file_path)
-            bus_samp.append((audio, fs))
+    def normalize_sample(self, samples):
+        normalized_samples = []
+        for sample in samples:
+            normalized = lb.util.normalize(sample[0])
+            normalized_samples.append((normalized, sample[1]))
+        return normalized_samples
 
-    return bus_samp
+    def get_energy(self, normalized_data):
+        energy_data = []
+        for audio in normalized_data:
+            signal, sample_rate = audio
+            energy = np.array([
+                sum(abs(signal[i:i+self.frame_length]**2))
+                for i in range(0, len(signal), self.hop_length)
+            ])
+            energy_data.append(energy)
+        return energy_data
+    
+    def get_mfcc(self, normalized_data):
+        mfccs_data = []
 
-def get_tram_samples():
-    tram_samp = []
+        for audio in normalized_data:
 
-    for filename in os.listdir('tram_samples'):
-        file_path = os.path.join('tram_samples', filename)
-        if os.path.isfile(file_path):
-            audio, fs = read_file(file_path)
-            tram_samp.append((audio, fs))
-    return tram_samp
+            signal = audio[0]
+            sample_rate = audio[1]
+            
+            mfccs = lb.feature.mfcc(y=signal, sr=sample_rate)
+            mfccs_data.append((mfccs, sample_rate))
+        
+        return mfccs_data
+    
+    def pad_mfccs(self, mfccs_data, pad_size):
+        padded_data = []
+        for mfccs, _ in mfccs_data:
+            pad_width = max(0, pad_size - mfccs.shape[1])
+            mfccs_padded = np.pad(mfccs, pad_width=((0,0), (0,pad_width)), mode='constant')
+            padded_data.append(mfccs_padded.flatten()[:pad_size])
+        
+        return padded_data
+    
+    
 
+# Example usage
+processor = AudioProcessor()
+train_bus = processor.read_files_from_folder('37007__aleksin__bussound')
+train_tram = processor.read_files_from_folder('36822__hnminh__tram_class')
+val_bus = processor.read_files_from_folder('39926__thespicychip__tampere_bus_audio_data')
+val_tram = processor.read_files_from_folder('39927__thespicychip__tampere_tram_audio_data')
+test_bus = processor.read_files_from_folder('bus_samples')
+test_tram = processor.read_files_from_folder('tram_samples')
 
-tram_samples = get_tram_samples()
-bus_samples = get_bus_samples()
-
-
-# Normalize the data
-
-normalized_tram = []
-normalized_bus = []
-
-for sample in tram_samples:
-    normalized = lb.util.normalize(sample[0])
-    normalized_tram.append((normalized, sample[1]))
-
-for sample in bus_samples:
-    normalized = lb.util.normalize(sample[0])
-    normalized_bus.append((normalized, sample[1]))
-
+norm_train_bus = processor.normalize_sample(train_bus)
+norm_train_tram = processor.normalize_sample(train_tram)
+norm_val_bus = processor.normalize_sample(val_bus)
+norm_val_tram = processor.normalize_sample(val_tram)
+norm_test_bus = processor.normalize_sample(test_bus)
+norm_test_tram = processor.normalize_sample(test_tram)
 
 # Feature: energy
 
-def get_energy(normalized_data):
-
-    # En tiiä onks nää arvot hyvät/täytyyks olla ees hop_length
-    hop_length = 256
-    frame_length = 512
-    win_size = int(0.1*44100)
-
-    energy_data = []
-
-    for audio in normalized_data:
-
-        signal = audio[0]
-        sample_rate = audio[1]
-
-        energy = np.array([
-            sum(abs(signal[i:i+frame_length]**2))
-            for i in range(0, len(signal), hop_length)
-        ])
-
-        energy_data.append((energy, sample_rate))
-    
-    return energy_data
-
-tram_energy = get_energy(normalized_tram)
-bus_energy = get_energy(normalized_bus)
+tram_energy = processor.get_energy(norm_test_tram)
+bus_energy = processor.get_energy(norm_test_bus)
 
 plt.figure()
 plt.hist(tram_energy[2][0], bins=20, color='red', edgecolor='black', alpha=0.7, label='Tram')
@@ -111,15 +124,13 @@ def get_rms(normalized_data):
 
     return rms_data
 
-tram_rms = get_rms(normalized_tram)
-bus_rms = get_rms(normalized_bus)
+tram_rms = get_rms(norm_test_tram)
+bus_rms = get_rms(norm_test_bus)
 
 plt.figure()
 plt.hist(tram_rms[10][0].flatten(), bins=20, color='red', edgecolor='black', alpha=0.7, label='Tram')
 plt.hist(bus_rms[6][0].flatten(), bins=20, color='blue', edgecolor='black', alpha=0.7, label='Bus')
 plt.legend()
-
-
 
 # For spectrograms
 
@@ -162,16 +173,16 @@ def get_spectrogram(normalized_data):
     return spectrogram_data, log_spectrogram_data
 
 
-tram_spec, tram_logspec = get_spectrogram(normalized_tram)
-bus_spec, bus_logspec = get_spectrogram(normalized_bus)
+tram_spec, tram_logspec = get_spectrogram(norm_test_tram)
+bus_spec, bus_logspec = get_spectrogram(norm_test_bus)
 
 # Ei toimi nää plottaukset 
 
-plot_spectrogram(tram_spec[6], sample_rate=tram_samples[6][1])
-plot_spectrogram(tram_logspec[6], sample_rate=tram_samples[6][1])
+plot_spectrogram(tram_spec[6], sample_rate=test_tram[6][1])
+plot_spectrogram(tram_logspec[6], sample_rate=test_tram[6][1])
 
-plot_spectrogram(bus_spec[6], sample_rate=bus_samples[6][1])
-plot_spectrogram(bus_logspec[6], sample_rate=bus_samples[6][1])
+plot_spectrogram(bus_spec[6], sample_rate=test_bus[6][1])
+plot_spectrogram(bus_logspec[6], sample_rate=test_bus[6][1])
 
 
 # Feature: mel-spectrograms and logmel-spectrograms
@@ -195,42 +206,28 @@ def get_mel_spectrogram(normalized_data):
     return mel_spectrogram_data, logmel_spectrogram_data
 
 
-tram_melspec, tram_logmelspec = get_mel_spectrogram(normalized_tram)
-bus_melspec, bus_logmelspec = get_mel_spectrogram(normalized_bus)
+tram_melspec, tram_logmelspec = get_mel_spectrogram(norm_test_tram)
+bus_melspec, bus_logmelspec = get_mel_spectrogram(norm_test_tram)
 
 
 # Eikä nämäkään
 
-plot_spectrogram(tram_melspec[6], sample_rate=tram_samples[6][1])
-plot_spectrogram(tram_logmelspec[6], sample_rate=tram_samples[6][1])
+plot_spectrogram(tram_melspec[6], sample_rate=test_tram[6][1])
+plot_spectrogram(tram_logmelspec[6], sample_rate=test_tram[6][1])
 
-plot_spectrogram(bus_melspec[6], sample_rate=bus_samples[6][1])
-plot_spectrogram(bus_logmelspec[6], sample_rate=bus_samples[6][1])
+plot_spectrogram(bus_melspec[6], sample_rate=test_bus[6][1])
+plot_spectrogram(bus_logmelspec[6], sample_rate=test_bus[6][1])
 
-
-
-# Feature: MFCC
-
-def get_mfcc(normalized_data):
-
-    mfccs_data = []
-
-    for audio in normalized_data:
-
-        signal = audio[0]
-        sample_rate = audio[1]
-        
-        mfccs = lb.feature.mfcc(y=signal, sr=sample_rate)
-        mfccs_data.append((mfccs, sample_rate))
-    
-    return mfccs_data
+tram_mfccs_test = processor.get_mfcc(norm_test_tram)
+bus_mfccs_test = processor.get_mfcc(norm_test_bus)
+tram_mfccs_train = processor.get_mfcc(norm_train_tram)
+bus_mfccs_train = processor.get_mfcc(norm_train_bus)
+tram_mfccs_val = processor.get_mfcc(norm_val_tram)
+bus_mfccs_val = processor.get_mfcc(norm_val_bus)
 
 
-tram_mfccs = get_mfcc(normalized_tram)
-bus_mfccs = get_mfcc(normalized_bus)
-
-plot_spectrogram(tram_mfccs[6][0], sample_rate=tram_samples[6][1])
-plot_spectrogram(bus_mfccs[6][0], sample_rate=bus_samples[6][1])
+plot_spectrogram(tram_mfccs_test[6][0], sample_rate=test_tram[6][1])
+plot_spectrogram(bus_mfccs_test[6][0], sample_rate=test_bus[6][1])
 
 
 # Feature: CQT spectrogram
@@ -250,57 +247,60 @@ def get_cqt(normalized_data):
     return cqt_data
 
 
-tram_cqts = get_cqt(normalized_tram)
-bus_cqts = get_cqt(normalized_bus)
+tram_cqts = get_cqt(norm_test_tram)
+bus_cqts = get_cqt(norm_test_bus)
 
 # Eikä muuten tässäkään
-plot_spectrogram(tram_cqts[6], sample_rate=tram_samples[6][1])
-plot_spectrogram(bus_cqts[6], sample_rate=bus_samples[6][1])
-
-
-
-# Function to pad MFCCs to a fixed size
-def pad_mfccs(mfccs_data, pad_size):
-    padded_data = []
-    for mfccs, _ in mfccs_data:
-        pad_width = max(0, pad_size - mfccs.shape[1])
-        mfccs_padded = np.pad(mfccs, pad_width=((0,0), (0,pad_width)), mode='constant')
-        padded_data.append(mfccs_padded.flatten()[:pad_size])
-    return padded_data
+plot_spectrogram(tram_cqts[6], sample_rate=test_tram[6][1])
+plot_spectrogram(bus_cqts[6], sample_rate=test_bus[6][1])
 
 # Determine the maximum size needed for padding
-max_size = max(max(mfccs.shape[1] for mfccs, _ in tram_mfccs),
-               max(mfccs.shape[1] for mfccs, _ in bus_mfccs))
+max_size = max(max(mfccs.shape[1] for mfccs, _ in tram_mfccs_test),
+               max(mfccs.shape[1] for mfccs, _ in bus_mfccs_test))
 
-padded_tram_mfccs = pad_mfccs(tram_mfccs, max_size)
-padded_bus_mfccs = pad_mfccs(bus_mfccs, max_size)
+padded_tram_mfccs_train = processor.pad_mfccs(tram_mfccs_train, max_size)
+padded_bus_mfccs_train = processor.pad_mfccs(bus_mfccs_train, max_size)
+padded_tram_mfccs_test = processor.pad_mfccs(tram_mfccs_test, max_size)
+padded_bus_mfccs_test = processor.pad_mfccs(bus_mfccs_test, max_size)
+padded_tram_mfccs_val = processor.pad_mfccs(tram_mfccs_val, max_size)
+padded_bus_mfccs_val = processor.pad_mfccs(bus_mfccs_val, max_size)
+
 
 # Labeling
-tram_labels = [0] * len(padded_tram_mfccs)
-bus_labels = [1] * len(padded_bus_mfccs)
+def label_data(bus_data, tram_data, bus_label, tram_label):
+    bus_labels = [bus_label] * len(bus_data)
+    tram_labels = [tram_label] * len(tram_data)
+    X = bus_data + tram_data
+    y = bus_labels + tram_labels
+    return X, y
 
+# Label datasets
+X_train, y_train = label_data(padded_bus_mfccs_train, padded_tram_mfccs_train, 1, 0)
+X_val, y_val = label_data(padded_bus_mfccs_val, padded_tram_mfccs_val, 1, 0)
+X_test, y_test = label_data(padded_bus_mfccs_test, padded_tram_mfccs_test, 1, 0)
 
-X = padded_bus_mfccs + padded_tram_mfccs  
-y = tram_labels + bus_labels 
-
-# Train SVM
-X = np.array(X)
-y = np.array(y)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 svm_classifier = SVC(kernel='linear')
 svm_classifier.fit(X_train, y_train)
 
+# Validation evaluation
+val_predictions = svm_classifier.predict(X_val)
+val_accuracy = accuracy_score(y_val, val_predictions)
+val_tn, val_fp, val_fn, val_tp = confusion_matrix(y_val, val_predictions).ravel()
+val_precision_score = val_tp / (val_tp + val_fp)
+val_recall_score = val_tp / (val_tp + val_fn)
 
-# Evaluate
-predictions = svm_classifier.predict(X_test)
-accuracy = accuracy_score(y_test, predictions)
-tn, fp, fn, tp = confusion_matrix(y_test, predictions).ravel()
-precision_score = tp / (tp + fp)
-recall_score = tp / (tp + fn)
+print("Validation Accuracy: " + "{:.2f}".format(val_accuracy))
+print("Validation Precision: " + "{:.2f}".format(val_precision_score))
+print("Validation Recall score: " + "{:.2f}".format(val_recall_score))
 
-print("Accuracy: " + "{:.2f}".format(accuracy))
-print("Precision: " + "{:.2f}".format(precision_score))
-print("Recall score:", recall_score)
+# Evaluating on test set
+test_predictions = svm_classifier.predict(X_test)
+test_accuracy = accuracy_score(y_test, test_predictions)
+tn, fp, fn, tp = confusion_matrix(y_test, test_predictions).ravel()
+test_precision_score = tp / (tp + fp)
+test_recall_score = tp / (tp + fn)
 
+print("Test Accuracy: " + "{:.2f}".format(test_accuracy))
+print("Test Precision: " + "{:.2f}".format(test_precision_score))
+print("Test Recall score: " + "{:.2f}".format(test_recall_score))
